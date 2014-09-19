ac2e1036d223e914d68bde44d86cde7cdde21b16
Ken Egozi <mail@kenegozi.com>
2014-09-18 17:00:01 -0700

    feedburner link, tags, and more

--- _assets/javascripts/site.js
+++ _assets/javascripts/site.js
@@ -1,13 +1,16 @@
 $(function() {
 	var header = $(".navbar-top");
+	var backToTopButton = $('.back-to-top');
 	var sections = $("section").map(function(i, s) { return {sectionId:s.id, top:$(s).position().top}; });
 	var menuItems = $('#main-nav li');
 	$(window).scroll(function() {
 		var scroll = $(window).scrollTop();
 		if (scroll >= 50) {
 			header.removeClass('navbar-top').addClass("navbar-top-dark");
+			backToTopButton.css('opacity', 0.4);
 		} else {
 			header.removeClass("navbar-top-dark").addClass('navbar-top');
+			backToTopButton.css('opacity', 0);
 		}
 		if (location.pathname === '/') {
 			var lastOne = sections[0];
